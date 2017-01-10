10aec9f82d81c7096846a2849f2cc933f652ec31
DAATConsultancy <daatconsultancy@gmail.com>
2017-01-10 17:07:48 +0100

    Update to quote count

--- import_validation.py
+++ import_validation.py
@@ -3,9 +3,12 @@
 
 import os
 import pyparsing as pp
+import time
+
+start_time = time.time()
 
 # Set working directory
-dump = "TEST IMPORT"
+dump = "20161216"
 datadir = "/Users/donaldposthuma/Documents/Klanten/ExperienceData/NationaleNederlanden/Letsel/0.Data/echo_dumps/" + dump
 os.chdir(datadir)
 
@@ -18,18 +21,28 @@ outputpath = "importcheck/"
 if not os.path.exists(outputpath):
     os.makedirs(outputpath)
 
+# Create folder to store validated data
+validatedpath = "validateddata/"
+if not os.path.exists(validatedpath):
+    os.makedirs(validatedpath)
+
 # Declare delimiter, quoter, content and field later used to construct the parser
 delimiter = ';'
 quote = '"'
-content = quote + pp.Word(pp.printables + ' ’', excludeChars=';"') + quote
+content = quote + pp.Word(pp.printables + ' -’€éß', excludeChars=';"') + quote
+# content = quote + pp.Word(pp.printables + ' ', excludeChars=';"') + quote
 emptycontent = quote + quote
 field = content | emptycontent
 
 
 for file in os.listdir(inputpath):
     filename = os.path.splitext(file)[0]
+    print file
     inputfile = file
     outputfile = 'IO_error_' + filename + '.txt'
+    log = 'Import Validation Log ' + dump + '.txt'
+
+    logfile = open(outputpath + log, 'w')
 
     with open(inputpath+inputfile, 'rb') as csvfile:
 
@@ -37,27 +50,44 @@ for file in os.listdir(inputpath):
             # Add LineEnd at the end of the parser
             firstline = csvfile.readline()
             count_delimiters = firstline.count(delimiter)
+            count_quotes_header = firstline.count(quote)
 
             rowparse = field
             for x in range (0,count_delimiters):
                 rowparse = rowparse + delimiter + field
 
             rowparse = rowparse + pp.LineEnd()
-
             # Loop trough lines to check whether they comply with parser
             reader = csvfile.readlines()
             outfile = open(outputpath + outputfile, 'w')
+            outfile.write(firstline)
+            lines_in_file = 0
+            errors_in_file = 0
             for idx, line in enumerate(reader):
-                try:
-                    rowparse.parseString(line)
-                except:
-                    print idx
-                    errorstring =  str(idx+2) + ': ' + line
+                count_quotes_line = line.count(quote)
+                lines_in_file = lines_in_file + 1
+
+                if count_quotes_header == count_quotes_line:
+                    pass
+                else:
+                    errors_in_file = errors_in_file + 1
+                    errorstring = str(idx + 2) + ': ' + line
+                    #errorstring = line
                     outfile.write(errorstring)
+                # try:
+                #     rowparse.parseString(line)
+                # except:
+                #     errors_in_file = errors_in_file+1
+                #     errorstring =  str(idx+2) + ': ' + line
+                #     outfile.write(errorstring)
+
+            print filename+': lines = '+str(lines_in_file)+', errors = '+str(errors_in_file) + ' (' + str(round(100*errors_in_file/(lines_in_file+0.00001), 1))+ '%)'
+            logfile.close()
 
             outfile.close()
-
-            # Delete output files which contain no errors
-            if os.stat(outputpath+outputfile).st_size == 0:
-                os.remove(outputpath+outputfile)
+            print 'runtime: ' + str(time.time()-start_time)
+            #MIf no errors then move file to validated path and delete output file
+            if errors_in_file == 0:
+                os.remove(outputpath + outputfile)
+                os.rename(inputpath+inputfile, validatedpath+inputfile)
 
